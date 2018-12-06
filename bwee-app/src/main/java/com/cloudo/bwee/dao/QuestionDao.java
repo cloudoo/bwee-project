2b146b2a37fcd0faa005364f197d3f95faecb95f
cloudoo <leisy@csair.com>
2018-12-06 15:54:08 +0800

    1.添加ui相关

--- bwee-app/src/main/java/com/cloudo/bwee/dao/QuestionDao.java
+++ bwee-app/src/main/java/com/cloudo/bwee/dao/QuestionDao.java
@@ -1,7 +1,9 @@
 package com.cloudo.bwee.dao;
 
+import com.cloudo.bwee.domain.Question;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionDao {
+    public int insert(Question question);
 }
