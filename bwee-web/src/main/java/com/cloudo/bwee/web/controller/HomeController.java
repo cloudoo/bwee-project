723ebf6c1abbe37db20ef726b6c47d8702b4dc65
cloudoo <leisy@csair.com>
2018-12-17 11:30:17 +0800

    1.xamdin template

--- bwee-web/src/main/java/com/cloudo/bwee/web/controller/HomeController.java
+++ bwee-web/src/main/java/com/cloudo/bwee/web/controller/HomeController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/home")
 public class HomeController {
 
 
-    @RequestMapping("/home.bw")
     public ModelAndView home(){
-
         return new ModelAndView("index");
     }
 
