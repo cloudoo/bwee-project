d79543d8f17b0896a87ef7e2164defb2d23efbf7
leisy <leisy@csair.com>
2019-09-27 17:17:51 +0800

    1.application commit

---
+++ bwee-app/src/main/java/com/cloudo/bwee/app/Application.java
@@ -0,0 +1,31 @@
+package com.cloudo.bwee.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.cloudo.bwee")
+@EnableJpaRepositories(basePackages = "com.cloudo.bwee.repository")
+@EntityScan("com.csair.dt.*.domain")
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+//    @Bean
+//    public CommandLineRunner demo(SysInfoRepository sysInfoRepository){
+//        return (args) -> {
+//
+//        }
+//    }
+
+
+}
