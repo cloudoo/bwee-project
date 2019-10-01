4ee23ecd8429de4fe97febd3eb5b87d977e558dd
cloudoo <8412893@qq.com>
2019-10-01 20:24:16 +0800

    1.修复spring boot web 不能启动的问题

--- bwee-boot-web/src/main/java/com/cloudo/bwee/web/WebApplication.java
+++ bwee-boot-web/src/main/java/com/cloudo/bwee/web/WebApplication.java
@@ -1,9 +1,13 @@
 package com.cloudo.bwee.web;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cloudo.bwee")
+@EnableDubbo
 public class WebApplication {
 
     public static void main(String[] args) {
